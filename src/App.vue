a76dc235d568aa7aadb64e2aaf89080ce9935e76
RobertoVz <roberto@opalo.studio>
2020-03-21 18:01:32 -0600

    listo

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
-
 <style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=B612|Barrio&display=swap')
 #app
-  font-family: Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
+  //font-family: 'Barrio', cursive
+  //font-family: 'B612', sans-serif
+  //-webkit-font-smoothing: antialiased
+  //-moz-osx-font-smoothing: grayscale
+  //color: #2c3e50
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="Fondo d-flex flex-row align-center justify-center">
+    <img alt="logo" src="../assets/logo.png" />
+    <v-container>
+      <v-btn>Nosotros</v-btn>
+      <v-btn>Proyectos</v-btn>
+      <v-btn>Contacto</v-btn>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    //HelloWorld
   }
-};
+}
 </script>
+<style lang="sass" scoped>
+.Fondo
+  width: 100vw
+  height: 100vh
+</style>
