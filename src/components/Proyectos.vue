feb5cd87aa95f7e60cfc3968053c5377f4c5b63b
RobertoVz <roberto@opalo.studio>
2020-03-25 21:55:53 -0600

    listo intervencion ilegal

--- src/components/Proyectos.vue
+++ src/components/Proyectos.vue
@@ -2,104 +2,34 @@
   <v-expansion-panels focusable>
     <v-expansion-panel>
       <v-expansion-panel-header class="d-flex flex-column">
-        <b>Diciembre 2013</b> “Alas y Raíces Baúl poblano” Museo de Arte
-        Popular, Ciudad de México. Ciudad de México. Artista expositor.
-        Diseñador y decorador.
+        <b>Diciembre 2019</b>Rótulo entrada Principal, Centro Escolar Aparicio,
+        21 y 22 de diciembre Centro de Convenciones Puebla y Centro Escolar
+        Aparicio
       </v-expansion-panel-header>
       <v-expansion-panel-content>
-        <v-img src="../assets/fondos/verde.jpg" />
+        <v-img src="../assets/portafolio/cea.jpeg" />
+        <v-img src="../assets/portafolio/cea2.jpeg" />
+        <v-img src="../assets/portafolio/cea4.jpeg" />
+        <v-img src="../assets/portafolio/cea5.jpeg" />
+        <v-img src="../assets/portafolio/cea6.jpeg" />
       </v-expansion-panel-content>
     </v-expansion-panel>
     <v-expansion-panel>
       <v-expansion-panel-header class="d-flex flex-column">
-        <b>Noviembre 2014 </b> “1° Encuentro vecinal académico sobre pintura
-        mural y graffiti” Junta auxiliar San Baltazar Campeche, Puebla. Puebla
-        México. Tallerista. Decorador mural y tallerista.
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <v-img src="../assets/fondos/verde.jpg" />
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-header class="d-flex flex-column">
-        <b>Enero 2015</b> “Intervención de contenedores” Espacio Cultural del
-        Mercado de Sabores Propuesto por el Instituto Municipal de Arte y
-        Cultura de Puebla (IMACP), Subdirección de Cultura para la No Violencia
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <v-img src="../assets/portafolio/illegal.png" />
-        <v-img src="../assets/portafolio/illegal2.png" />
-        <v-img src="../assets/portafolio/illegal3.png" />
-        <v-img src="../assets/portafolio/illegal4.png" />
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-header class="d-flex flex-column">
-        <b>Enero 2015 Enero 2015</b> “Intervención de contenedores” Espacio
-        Cultural del Mercado de Sabores Propuesto por el Instituto Municipal de
-        Arte y Cultura de Puebla (IMACP), Subdirección de Cultura para la No
-        Violencia
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <v-img src="../assets/portafolio/1.png" />
-        <v-img src="../assets/portafolio/2.png" />
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-header class="d-flex flex-column">
-        <b>Abril 2016 - Abril 2016</b> “nosotros mi barrio” Festival de
-        fotografía y arte, comunidad Cerro del Márquez Puebla Centro Comunitario
-        Kali. Puebla México. Tallerista y Decorador mural
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <v-img src="../assets/fondos/verde.jpg" />
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-header class="d-flex flex-column">
-        <b>Febrero 2016 - Enero 2016</b> “Artistas emergentes UDLAP muestra de
-        dibujo y gráfica expandida” Teatro principal Zócalo Puebla. Puebla
-        México. Artista expositor.
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <v-img src="../assets/fondos/verde.jpg" />
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-header class="d-flex flex-column">
-        <b>Marzo-2017-Marzo 2017</b> “Mural Homenaje al Pulque y Xipe-Totec” 4°
-        Festival de Equinoccio 2017 Gobierno de San Andrés Cholula Puebla México
-        2014-2018. Artista expositor. Decorador mural
+        <b>Noviembre 2019</b> “Proyecto Escenografía escenario Festival de
+        Música Kick of Love” 21 y 22 de noviembre Explanada Puebla
       </v-expansion-panel-header>
       <v-expansion-panel-content>
         <iframe
           width="100%"
           height="500"
-          src="https://www.youtube.com/embed/6z9-NccU4Zk"
+          src="https://www.youtube.com/embed/A2vBNVhqZ3k"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
         ></iframe>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-header class="d-flex flex-column">
-        <b>Junio 2018-Agosto 2018</b> “Remodelación de interiores Hostel la
-        Posada” Santa Teresa Costa Rica. Diseñador y Decorador mural. Fachada
-        interior, recepción, y cocina.
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <v-img src="../assets/fondos/verde.jpg" />
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-header class="d-flex flex-column">
-        <b>Septiembre 2018 - Octubre 2018</b> “Remodelación mural nómada”
-        Universidad de las Américas Puebla. Decoración Mural.
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <v-img src="../assets/portafolio/nomada.png" />
-        <v-img src="../assets/portafolio/nomada2.png" />
+        <v-img src="../assets/portafolio/kick.jpeg" />
+        <v-img src="../assets/portafolio/kick2.jpeg" />
       </v-expansion-panel-content>
     </v-expansion-panel>
     <v-expansion-panel>
@@ -142,36 +72,57 @@
         <v-img src="../assets/portafolio/teca15.jpeg" />
       </v-expansion-panel-content>
     </v-expansion-panel>
+
     <v-expansion-panel>
       <v-expansion-panel-header class="d-flex flex-column">
-        <b>Noviembre 2019</b> “Proyecto Escenografía escenario Festival de
-        Música Kick of Love” 21 y 22 de noviembre Explanada Puebla
+        <b>Septiembre 2018 - Octubre 2018</b> “Remodelación mural nómada”
+        Universidad de las Américas Puebla. Decoración Mural.
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-img src="../assets/portafolio/nomada.png" />
+        <v-img src="../assets/portafolio/nomada2.png" />
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-header class="d-flex flex-column">
+        <b>Marzo-2017-Marzo 2017</b> “Mural Homenaje al Pulque y Xipe-Totec” 4°
+        Festival de Equinoccio 2017 Gobierno de San Andrés Cholula Puebla México
+        2014-2018. Artista expositor. Decorador mural
       </v-expansion-panel-header>
       <v-expansion-panel-content>
         <iframe
           width="100%"
           height="500"
-          src="https://www.youtube.com/embed/A2vBNVhqZ3k"
+          src="https://www.youtube.com/embed/6z9-NccU4Zk"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
         ></iframe>
-        <v-img src="../assets/portafolio/kick.jpeg" />
-        <v-img src="../assets/portafolio/kick2.jpeg" />
       </v-expansion-panel-content>
     </v-expansion-panel>
     <v-expansion-panel>
       <v-expansion-panel-header class="d-flex flex-column">
-        <b>Diciembre 2019</b>Rótulo entrada Principal, Centro Escolar Aparicio,
-        21 y 22 de diciembre Centro de Convenciones Puebla y Centro Escolar
-        Aparicio
+        <b>2015 - 2016</b> Beneficiario del Programa de Estímulo a la Creación y
+        al Desarrollo Artístico de Puebla categoría: artistas con trayectoria
+        2015-2016 proyecto de autoría: Intervención I Legal.
       </v-expansion-panel-header>
       <v-expansion-panel-content>
-        <v-img src="../assets/portafolio/cea.jpeg" />
-        <v-img src="../assets/portafolio/cea2.jpeg" />
-        <v-img src="../assets/portafolio/cea4.jpeg" />
-        <v-img src="../assets/portafolio/cea5.jpeg" />
-        <v-img src="../assets/portafolio/cea6.jpeg" />
+        <v-img src="../assets/portafolio/illegal.png" />
+        <v-img src="../assets/portafolio/illegal2.png" />
+        <v-img src="../assets/portafolio/illegal3.png" />
+        <v-img src="../assets/portafolio/illegal4.png" />
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-header class="d-flex flex-column">
+        <b>Enero 2015 Enero 2015</b> “Intervención de contenedores” Espacio
+        Cultural del Mercado de Sabores Propuesto por el Instituto Municipal de
+        Arte y Cultura de Puebla (IMACP), Subdirección de Cultura para la No
+        Violencia
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-img src="../assets/portafolio/1.png" />
+        <v-img src="../assets/portafolio/2.png" />
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
